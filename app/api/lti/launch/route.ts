1fa2db4030c27d9336666b6509c835277dbe3b09
Miles Exner <mexner@pacificcollege.edu>
2024-06-20 19:41:22 -0700

    🚧 updated auth logic

--- app/api/lti/launch/route.ts
+++ app/api/lti/launch/route.ts
@@ -1,5 +1,7 @@
 import { jwtVerify, createRemoteJWKSet } from 'jose';
 import { signJwt } from '@/app/utils/jwt';
+import { randomBytes } from 'crypto';
+import { redirect } from 'next/dist/server/api-utils';
 
 const jwksUrl = process.env.JWKS_URL || '';
 if (!jwksUrl) {
@@ -8,7 +10,7 @@ if (!jwksUrl) {
 const JWKS_URI = new URL(jwksUrl);
 const JWKS = createRemoteJWKSet(JWKS_URI);
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<Response> {
   const url = new URL(request.url);
   const iss = url.searchParams.get('iss');
   const login_hint = url.searchParams.get('login_hint');
@@ -24,6 +26,77 @@ export async function GET(request: Request) {
     });
   }
 
+  const state = randomBytes(16).toString('hex');
+  const nonce = randomBytes(16).toString('hex');
+
+  // Save state and nonce in cookies
+  const headers = new Headers();
+  headers.append('Set-Cookie', `lti_state=${state}; Path=/; HttpOnly; Secure`);
+  headers.append('Set-Cookie', `lti_nonce=${nonce}; Path=/; HttpOnly; Secure`);
+
+  const clientId = process.env.CLIENT_ID;
+  const authUrl = `${iss}/auth`;
+  if (!clientId) {
+    return new Response(JSON.stringify({ message: 'Client ID not found in environment variables' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const authRedirectUrl = new URL(authUrl);
+  authRedirectUrl.searchParams.set('scope', 'openid');
+  authRedirectUrl.searchParams.set('response_type', 'id_token');
+  authRedirectUrl.searchParams.set('client_id', clientId);
+  authRedirectUrl.searchParams.set('redirect_uri', target_link_uri);
+  authRedirectUrl.searchParams.set('login_hint', login_hint);
+  authRedirectUrl.searchParams.set('state', state);
+  authRedirectUrl.searchParams.set('response_mode', 'form_post');
+  authRedirectUrl.searchParams.set('nonce', nonce);
+  authRedirectUrl.searchParams.set('prompt', 'none');
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      ...headers,
+      'Location': authRedirectUrl.toString(),
+    },
+  });
+}
+
+export async function POST(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  const state = url.searchParams.get('state');
+  const id_token = url.searchParams.get('id_token');
+  const redirectUri = process.env.REDIRECT_URL;
+
+  if (!redirectUri) {
+    throw new Error('Redirect URL not found in environment variables');
+  }
+
+  if (!state || !id_token) {
+    return new Response(JSON.stringify({ message: 'Missing required parameters' }), {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const cookies = request.headers.get('cookie');
+  const savedState = cookies?.split('; ').find(row => row.startsWith('lti_state='))?.split('=')[1];
+  const savedNonce = cookies?.split('; ').find(row => row.startsWith('lti_nonce='))?.split('=')[1];
+
+  if (state !== savedState) {
+    return new Response(JSON.stringify({ message: 'Invalid state' }), {
+      status: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   try {
     const audience = process.env.AUDIENCE || '';
     const issuer = process.env.ISSUER || '';
@@ -31,8 +104,15 @@ export async function GET(request: Request) {
       throw new Error('Audience or issuer not found in environment variables');
     }
 
-    // Verifying the lti_message_hint token
-    const { payload } = await jwtVerify(lti_message_hint, JWKS, { issuer, audience });
+    const { payload } = await jwtVerify(id_token, JWKS, {
+      issuer,
+      audience,
+    });
+
+    // Clear the state and nonce cookies
+    const headers = new Headers();
+    headers.append('Set-Cookie', 'lti_state=; Path=/; Max-Age=0');
+    headers.append('Set-Cookie', 'lti_nonce=; Path=/; Max-Age=0');
 
     // Create a response JWT signed with the private key
     const responsePayload = {
@@ -43,16 +123,17 @@ export async function GET(request: Request) {
 
     const signedJwt = await signJwt(responsePayload);
 
-    const dashboardUrl = process.env.DASHBOARD_URL || '';
-    if (!dashboardUrl) {
-      throw new Error('Dashboard URL not found in environment variables');
-    }
-    const redirectUrl = new URL(dashboardUrl);
+    const redirectUrl = new URL(redirectUri);
     redirectUrl.searchParams.set('token', signedJwt);
 
-    return Response.redirect(redirectUrl.toString(), 302);
+    headers.append('Location', redirectUrl.toString());
+
+    return new Response(null, {
+      status: 302,
+      headers,
+    });
   } catch (error) {
-    return new Response(JSON.stringify({ message: error }), {
+    return new Response(JSON.stringify({ message: 'Invalid ID Token', error }), {
       status: 401,
       headers: {
         'Content-Type': 'application/json',
