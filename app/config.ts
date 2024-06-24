e03e79aa2bdbb76f6176afa77a328b10ff5f2456
Miles Exner <mexner@pacificcollege.edu>
2024-06-24 12:39:11 -0700

    🚚 moved global variables

---
+++ app/config.ts
@@ -0,0 +1,5 @@
+export const ALLOWED_ROLES = [
+  "http://purl.imsglobal.org/vocab/lis/v2/system/person#Administrator",
+  "http://purl.imsglobal.org/vocab/lis/v2/membership#Administrator",
+  "http://purl.imsglobal.org/vocab/lis/v2/institution/person#Instructor"
+];
