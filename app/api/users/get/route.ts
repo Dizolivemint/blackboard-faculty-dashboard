a4befee3b94b46bf436a5f470692a7b5d02391d8
Miles Exner <mexner@pacificcollege.edu>
2024-06-26 16:23:09 -0700

    🚀 added grade table

---
+++ app/api/users/get/route.ts
@@ -0,0 +1,104 @@
+import { verifyJwt } from '@/app/utils/jwt';
+import { ALLOWED_ROLES } from '@/app/config';
+import Blackboard from '@/app/integrations/blackboard';
+
+const BB_API_URL = process.env.AUDIENCE || '';
+
+export async function POST(request: Request): Promise<Response> {
+  const { token, courseId } = await request.json();
+
+  if (!token || !courseId) {
+    return new Response(JSON.stringify({ message: 'Missing required parameters' }), {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  if (!BB_API_URL) {
+    return new Response(JSON.stringify({ message: 'Blackboard API URL not found' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  try {
+    const payload = await verifyJwt(token);
+
+    const roles = payload['https://purl.imsglobal.org/spec/lti/claim/roles'] as Array<string>;
+    const hasAllowedRole = roles && roles.some(role => ALLOWED_ROLES.includes(role));
+
+    if (!hasAllowedRole) {
+      return new Response(JSON.stringify({ message: 'Access denied. You do not have the required role.' }), {
+        status: 403,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const blackboard = Blackboard.getInstance();
+    await blackboard.init();
+
+    // Get course ID
+    if (!courseId) {
+      return new Response(JSON.stringify({ message: 'Course ID not found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    // Get overall grade and final grade column IDs
+    const overallColumnId = await blackboard.getGradeColumnId(courseId, 'Overall Grade');
+    const finalColumnId = await blackboard.getGradeColumnId(courseId, 'Final Grade');
+    
+    // Get overall grade and final grade column rows
+    if (!overallColumnId || !finalColumnId) {
+      return new Response(JSON.stringify({ message: 'Grade column ID not found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+    const overallResponse = await blackboard.getGradeColumnUsers(courseId, overallColumnId);
+    const finalResponse = await blackboard.getGradeColumnUsers(courseId, finalColumnId);
+
+    if (!overallResponse || !finalResponse) {
+      return new Response(JSON.stringify({ message: 'No grades found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const overall = overallResponse.results;
+    const final = finalResponse.results;
+
+    const grades = {
+      overall,
+      final
+    };
+
+    return new Response(JSON.stringify(grades), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    return new Response(JSON.stringify({ message: 'Internal server error', error }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,44 +6,64 @@ import { useEffect, useState } from 'react';
 import { JWTClaims } from '@/app/models/jwt';
 import jwt from 'jsonwebtoken';
 import { ALLOWED_ROLES } from '@/app/config';
-
-const publicKey = process.env.PUBLIC_JWK || '';
+import { GradebookColumnUser } from '@/app/models/blackboard';
 
 const Dashboard = () => {
   const token = useSearchParams().get('token');
   const [userData, setUserData] = useState<JWTClaims | null>(null);
+  const [grades, setGrades] = useState<{ overall: Array<GradebookColumnUser>; final: Array<GradebookColumnUser> }| null>(null);
+  const [students, setStudents] = useState<Array<GradebookColumnUser>>([]);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (!publicKey) {
-      setError('Public JWK not found in environment variables');
-      return;
-    }
-    if (token) {
-      localStorage.setItem('jwtToken', token as string);
+    const verifyAndFetchData = async () => {
+      if (token) {
+        localStorage.setItem('jwtToken', token as string);
 
-      try {
-        const verified = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
-        if (!verified) {
+        try {
+          const user = JSON.parse(atob((token as string).split('.')[1]));
+
+          const roles: Array<string> = user.roles;
+          const hasAllowedRole = roles && roles.some(role => ALLOWED_ROLES.includes(role));
+
+          if (!hasAllowedRole) {
+            setError('Access denied. You do not have the required role to access this dashboard.');
+          } else {
+            setUserData(user);
+            await fetchGrades(token, user.lis.course_section_sourcedid);
+          }
+        } catch (e) {
           setError('Invalid token.');
-          return;
         }
-
-        const user = JSON.parse(atob((token as string).split('.')[1]));
-
-        const roles: Array<string> = user.roles;
-        const hasAllowedRole = roles && roles.some(role => ALLOWED_ROLES.includes(role));
-
-        if (!hasAllowedRole) {
-          setError('Access denied. You do not have the required role to access this dashboard.');
-        } else {
-          setUserData(user);
-        }
-      } catch (e) {
-        setError('Invalid token.');
       }
-    }
+    };
+
+    verifyAndFetchData();
   }, [token]);
+
+  const fetchGrades = async (token: string, courseSectionSourcedid: string) => {
+    try {
+      const response = await fetch('/api/grades', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token,
+          course_section_sourcedid: courseSectionSourcedid,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch grades: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setGrades(data);
+    } catch (error) {
+      setError(`Error fetching grades: ${error}`);
+    }
+  };
 
   return (
     <div>
@@ -67,10 +87,67 @@ const Dashboard = () => {
             <h2>LIS</h2>
             <p>Person SourcedID: {userData.lis?.person_sourcedid}</p>
             <p>Course Section SourcedID: {userData.lis?.course_section_sourcedid}</p>
+            {grades && (
+              <SubmitGrade overallGrades={grades.overall} finalGrades={grades.final} />
+            )}
           </div>
         )
       )}
     </div>
+  );
+};
+
+const SubmitGrade = ({ overallGrades, finalGrades }: { overallGrades: Array<GradebookColumnUser>, finalGrades: Array<GradebookColumnUser> }) => {
+  return (
+    <div>
+      <input name="courseid" type="hidden" value="14157" />
+      <div className="box py-3">
+        <select id="block-grade-submission-populategrade" className="select custom-select menupopulategrade" name="populategrade">
+          <option selected={true} value="">Populate with overall grade...</option>
+        </select>
+      </div>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr className="top">
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              First name / Last name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Current Grade</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Final Grade</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {overallGrades.map((grade, index) => (
+            <StudentGradeRow
+              key={index}
+              studentName={grade.userId}
+              userId={grade.userId}
+              overallGrade={grade.score}
+              finalGrade={finalGrades[index].score}
+            />
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-4">
+        <input type="submit" value="Submit Grades" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+      </div>
+    </div>
+  );
+};
+
+const StudentGradeRow = ({ studentName, userId, overallGrade, finalGrade }: { studentName: string; userId: string; overallGrade: number, finalGrade: number }) => {
+  return (
+    <tr className="block-grade-submission-tr odd last">
+      <td className="px-6 py-4 whitespace-nowrap">
+        <input type="hidden" name="student" value={userId} />{studentName}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        {overallGrade}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <input className="border rounded px-2 py-1" id={`finalgrade_${userId}`} type="text" name={`finalgrade_${userId}`} size={5} value={finalGrade}/>
+      </td>
+    </tr>
   );
 };
 
